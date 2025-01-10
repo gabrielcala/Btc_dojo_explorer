91a586addedb5d480cb39d0d249739eb01f7be92
Mauricio Doerr <28199933+mauriciodoerr@users.noreply.github.com>
2025-01-10 20:42:42 -0300

    Update project configuration and enhance RPC handling (#1)
    - Configured Vite to use a proxy for RPC requests, redirecting to a specified target.
    - Modified Chamada component to use the proxy for fetching block data, including basic authentication and updated JSON-RPC version.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/rpc': {
+        target: 'http://137.131.145.51:18443',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/rpc/, '')
+      }
+    }
+  }
 })
